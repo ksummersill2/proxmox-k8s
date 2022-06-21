8dd3048704a9efc59ddf633f6b4d52d556bb74de
Kevin Summersill <kevin.summersill.ctr@us.af.mil>
2022-06-21 16:39:29 -0500

    updated with packer baseline for proxmox

--- main.tf
+++ main.tf
@@ -1,13 +1,29 @@
 resource "proxmox_vm_qemu" "k8s-master-1-node" {
   name        = "k8s-master-1-node"
+  desc        = "Utilized to setup an Ubuntu K8s Master Node"
   target_node = var.proxmox_host
+  vmid        = 401
   iso         = "pfSense-CE-2.5.1-RELEASE-amd64.iso"
-  agent = 0
-  os_type = "ubuntu"
-  cores = 2
-  sockets = 1
-  cpu = "host"
-  memory = 2048
-  scsihw = "virtio-scsi-pci"
-  bootdisk = "scsi0"
+  agent       = 1
+  os_type     = "other"
+  cores       = 2
+  sockets     = 1
+  cpu         = "host"
+  memory      = 2048
+  scsihw      = "virtio-scsi-pci"
+  bootdisk    = "scsi0"
+  sshkeys     = var.ssh_key
+
+  // Setting up the Network
+  network {
+    bridge = "vmbr0"
+    model  = "virtio"
+  }
+
+  disk {
+    storage = "local-lvm"
+    type    = "virtio"
+    size    = "100G"
+
+  }
 }
