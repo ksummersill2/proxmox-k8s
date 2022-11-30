14dbaa28cb5932148f0c07c5363c334e2f1b2584
Kevin Summersill <43754779+ksummersill2@users.noreply.github.com>
2022-11-29 21:59:01 -0600

    Update providers.tf
    Removed creds

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,8 +1,8 @@
 
 provider "proxmox" {
-  pm_api_url          = "https://10.0.125.81:8006/api2/json"
-  pm_api_token_id     = "yoda@pve!yoda"
-  pm_api_token_secret = "574fcf32-bd6b-4abe-908a-b186738b5bd9"
+  pm_api_url          = "https://<url to proxmox>:8006/api2/json"
+  pm_api_token_id     = "<your-pve>@pve!<your-pve>"
+  pm_api_token_secret = "<your secret for the api pve account>"
   pm_tls_insecure     = true
   pm_debug            = true
 }
